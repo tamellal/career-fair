bba0b6f32f0db450ea3ff35de3c75e7540802ba4
tamellal <36163467+tamellal@users.noreply.github.com>
2021-02-17 18:17:37 +0000

    Deploying to gh-pages from master @ ddce23bb5ac2e19e5cad90d9cc9cfe9ad3b269ad 🚀

---
+++ tsx/logos-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="logos-1" tilewidth="32" tileheight="32" tilecount="744" columns="12">
+ <image source="../tilesets/logos-1.png" width="400" height="2000"/>
+</tileset>

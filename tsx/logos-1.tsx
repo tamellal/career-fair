a5d2e3be6e743ee27c60883bfd3c3001253d9d5c
Toufik <t.amellal@thecodingmachine.com>
2021-02-17 21:00:40 +0100

    add some logos

--- tsx/logos-1.tsx
+++ tsx/logos-1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2021.01.13" name="logos-1" tilewidth="32" tileheight="32" tilecount="744" columns="12">
- <image source="../tilesets/logos-1.png" width="400" height="2000"/>
+<tileset version="1.4" tiledversion="2021.01.13" name="logos-1" tilewidth="32" tileheight="32" tilecount="966" columns="46">
+ <image source="../../ISP/tilesets/logos-1.png" width="1500" height="700"/>
 </tileset>
